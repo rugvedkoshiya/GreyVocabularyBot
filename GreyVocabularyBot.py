873e795b987f9380bedd814ccfd8e69888442fe5
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2021-06-28 14:59:03 +0530

    audio feature added

--- GreyVocabularyBot.py
+++ GreyVocabularyBot.py
@@ -6,7 +6,10 @@ from telegram import ParseMode
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 from datetime import tzinfo
-
+from gtts import gTTS
+import ffmpeg
+import logging
+from datetime import date
 
 def getRandomWord(jsonFile):
     randNumber = random.randrange(0, 817)
@@ -39,19 +42,44 @@ def writePreviousData(number):
     return previousData
 
 
-def runBot(auth, jsonFile):
+def convertToOGG():
+    output = "myWord." + 'ogg'
+    try:
+        stream = ffmpeg.input("myWord.mp3").output(output, ar='48000', ac=2, acodec='libopus', ab='32k', threads=2).overwrite_output()
+        ffmpeg.run(stream, quiet=True)
+        return output
+    except RuntimeError as e:
+        logging.error('An error occured whilst converting the file.', exc_info=e)
+
+
+def runBot(auth, jsonFile, sendTime):
     selectedWord, randNumber = getRandomWord(jsonFile)
-    print(selectedWord)
-    print(randNumber)
     increseCount(randNumber)
     wordIndexList = writePreviousData(randNumber)
 
-    response = ""
-    for wordIndex in wordIndexList:
-        response = "{}\n\n\n<b><i>{}</i></b>\n---------------\n{}".format(response,jsonFile[wordIndex]['word'],jsonFile[wordIndex]['meaning'])
+    # Send to channel
+    myWord = gTTS(text=jsonFile[wordIndexList[-1]]['word'], lang='en', slow=True, lang_check=True)
+    myWord.save("myWord.mp3")
+    myWord = open(convertToOGG(), "rb")
+    try:
+        telegram.Bot.sendAudio(auth, SETTING.CHANNEL_ID, audio=myWord, caption=f"<b><i>{jsonFile[len(wordIndexList)]['word']}</i></b>\n--------------\n{jsonFile[len(wordIndexList)]['meaning']}", parse_mode=ParseMode.HTML)
+    except telegram.error.BadRequest as e:
+        telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"Bad Request in Channel :)", parse_mode=ParseMode.HTML)
+    except telegram.error.Unauthorized as e:
+        telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"I am Unauthorized in Channel :)", parse_mode=ParseMode.HTML)
+    except Exception as e:
+        telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"Got Error in Channel :(\n\n{e}", parse_mode=ParseMode.HTML)
+    myWord.close()
+
+
+    # Send message to user about today
+    stopDate = date.today()
     for chatId in SETTING.CHAT_IDS:
         try:
-            telegram.Bot.sendMessage(auth, chatId, text=response, parse_mode=ParseMode.HTML)
+            if(sendTime == 0):
+                telegram.Bot.sendMessage(auth, chatId, text=f"<b>#Day{(stopDate - startDate).days + 1} &lt;10 AM&gt;\n{date.today().strftime('%d %b %Y')}</b>", parse_mode=ParseMode.HTML)
+            elif(sendTime == 1):
+                telegram.Bot.sendMessage(auth, chatId, text=f"<b>#Day{(stopDate - startDate).days + 1} &lt;5 PM&gt;\n{date.today().strftime('%d %b %Y')}</b>", parse_mode=ParseMode.HTML)
         except telegram.error.BadRequest as e:
             telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"{chatId} I don't know him/her :)", parse_mode=ParseMode.HTML)
         except telegram.error.Unauthorized as e:
@@ -61,20 +89,38 @@ def runBot(auth, jsonFile):
 
 
 
-def main():
+    # Individual send
+    for wordIndex in wordIndexList:
+        myWord = gTTS(text=jsonFile[wordIndex]['word'], lang='en', slow=True, lang_check=True)
+        myWord.save("myWord.mp3")
+        myWord = open(convertToOGG(), "rb")
+
+        for chatId in SETTING.CHAT_IDS:
+            try:
+                telegram.Bot.sendAudio(auth, chatId, audio=myWord, caption=f"<b><i>{jsonFile[wordIndex]['word']}</i></b>\n--------------\n{jsonFile[wordIndex]['meaning']}", parse_mode=ParseMode.HTML)
+            except telegram.error.BadRequest as e:
+                telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"{chatId} I don't know him/her :)", parse_mode=ParseMode.HTML)
+            except telegram.error.Unauthorized as e:
+                telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"{chatId} has blocked me :(", parse_mode=ParseMode.HTML)
+            except Exception as e:
+                telegram.Bot.sendMessage(auth, SETTING.ADMIN_ID, text=f"got error with {chatId} :(\n\n{e}", parse_mode=ParseMode.HTML)
+        myWord.close()
+
+
+
+def main(sendTime):
     try:
         auth = telegram.Bot(token=SETTING.TOKEN)
         with open("data.json", "r") as file:
             jsonFile = json.load(file)
-        runBot(auth, jsonFile)
+        runBot(auth, jsonFile, sendTime)
     except RecursionError as e:
         # print(e)
         auth = telegram.Bot(token=SETTING.TOKEN)
         runBot(auth, jsonFile)
 
-
+startDate = date.today()
 scheduler = BlockingScheduler()
-# scheduler.add_job(main, 'interval', hours=0.01)
-scheduler.add_job(main, trigger='cron', hour='4', minute='30')
-scheduler.add_job(main, trigger='cron', hour='11', minute='30')
+scheduler.add_job(lambda : main(0), trigger='cron', hour='4', minute='30')
+scheduler.add_job(lambda : main(1), trigger='cron', hour='11', minute='30')
 scheduler.start()
